31041b6f8a6401d81aed5b2389ab26f61da19c06
MamatNoobs <48646734+MamatNoobs@users.noreply.github.com>
2019-03-17 19:41:50 +0700

    utama.sh
    Update utama.sh

---
+++ utama.sh
@@ -0,0 +1,284 @@
+clear
+bi='\033[34;1m' #BIRU
+i='\033[32;1m' #IJO
+pur='\033[35;1m' #PURPLE
+cy='\033[36;1m' #CYAN
+me='\033[31;1m' #MERAH
+pu='\033[37;1m' #PUTIH
+ku='\033[33;1m' #KUNING
+echo
+echo
+python2 mamat.py
+echo
+clear
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"   
+sleep 1
+echo '''\a
+\033[34;1m               _
+\033[34;1m              / |
+\033[34;1m              | |
+\033[34;1m              | |
+\033[34;1m             _|_|_ '''
+sleep 1
+apt update && apt upgrade
+apt install nano
+apt install git
+pkg install python python2 vim figlet curl
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"
+sleep 1
+echo '''\a
+\033[32;1m              ____
+\033[32;1m             |___ \
+\033[32;1m               __) |
+\033[32;1m              / __/
+\033[32;1m             |_____| '''
+sleep 1
+apt install php
+pip2 install mechanize
+pip2 install lolcat
+pip2 install requests
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"
+sleep 1
+echo '''\a
+\033[35;1m             _____
+\033[35;1m            |___ /
+\033[35;1m              |_ \
+\033[35;1m             ___) |
+\033[35;1m            |____/ '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"
+sleep 1
+echo '''\a
+\033[33;1m             _  _
+\033[33;1m            | || |
+\033[33;1m            | || |_
+\033[33;1m            |__   _|
+\033[33;1m               |_| '''
+sleep 1
+clear
+echo
+echo
+echo $red" Tool akan di install dalam waktu 5 detik saja"
+echo $red" harap sabar menunggu boss :)"
+sleep 1
+echo '''\a
+\033[36;1m              ____
+\033[36;1m             | ___|
+\033[36;1m             |___ \
+\033[36;1m              ___) |
+\033[36;1m             |____/ '''
+sleep 1
+clear
+echo
+echo $cy"==================="$pur" ==================="
+echo $me"█"$bi"  Author  :  MamatNoobs          "$me"          █"
+echo $ku"█"$bi"  Facebook:  Mamat Utama"$ku"            █"
+echo $bi"█"$bi"  WhatsApp:  +6281272509038"$bi"    █"
+echo $i" █"$bi"  Gmail.  :  MamatUtama509@gmail.com"$i"             █"
+echo $me"==================="$ku" ==================="
+echo
+echo $i"-----› Menu BRUTEPORCE NEWS:"
+echo $cy"⟨"$pu"1"$cy"⟩"$bi" -» Report FB"
+echo $cy"⟨"$pu"2"$cy"⟩"$bi" -» Brute Force"
+echo $cy"⟨"$pu"3"$cy"⟩"$bi" -» Brute Force Facebook Cracker"
+echo $cy"⟨"$pu"4"$cy"⟩"$bi" -» Mbf"
+echo $cy"⟨"$pu"5"$cy"⟩"$bi" -» Cloning Yahoo"
+echo $cy"⟨"$pu"6"$cy"⟩"$bi" -» OsiF"
+echo $cy"⟨"$pu"7"$cy"⟩"$bi" -» Bot Reaction Facebook"
+echo $cy"⟨"$pu"8"$cy"⟩"$bi" -» Profileguard Facebook"
+echo $cy"⟨"$pu"9"$cy"⟩"$bi" -» Spam WhatsApp"
+echo $cy"⟨"$pu"10"$cy"⟩"$bi"-» Spam Call"
+echo $cy"⟨"$pu"11"$cy"⟩"$bi"-» Download Mp3"
+echo $cy"⟨"$pu"12"$cy"⟩"$bi"-» Youtube-dl"
+echo $cy"⟨"$pu"13"$cy"⟩"$bi"-» Mempercantik Tampilan Termux"
+echo $cy"⟨"$pu"14"$cy"⟩"$bi"-» Keluar"
+echo
+echo $pu"╭─["$i"Pilih Nomor Boss"$pu"]"
+read -p"╰─> : " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/3.git
+cd 3
+python2 bf.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/7.git
+cd 7
+python2 crack.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/1.git
+cd 1
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/2.git
+cd 2
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/8.git
+cd 8
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/5.git
+cd 5
+python2 blackbot.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/13.git
+cd 13
+php profile.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/12.git
+cd 12
+php whatsapp.php
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/4.git
+cd 4
+php SpamCall.php
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/Mp3_Downloader.git
+cd Mp3_Downloader
+pip install -r requirements.txt
+python Mp3_Downloader.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/6.git
+cd 6
+sh youtube-dl.sh
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "Lest G O ."|lolcat
+sleep 1
+git clone https://github.com/Edi-nasa47/9.git
+cd 9
+chmod +x termux.py
+python2 termux.py
+fi
+
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "E X I T"|Ulolcat
+sleep 2
+echo $i"SEMANGAT PAGI..."
+echo $i"Terima Kasih Akang sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan Akang di pakai semoga bermanfaat"
+sleep 2
+echo $i"Buat Akang²"
+sleep 2
+echo $i"Bila Akang belum mengerti Akang Bisa Nanya"
+echo $i"Melalui Via:"
+sleep 2
+echo $ku"Facebook :"$pur" Mamat Utama"
+echo $ku"Gmail.   :"$pur" MamatUtama509@gmail.com"
+echo $ku"WhatsApp :"$pur" 081272509038"
+sleep 2
+echo $i"dan jangan lupa "$cy" ADD FACEBOOK ADMIN"
+sleep 2
+echo $i"WhatsApp : "$cy" 081272509038"
+sleep 2
+echo $i"Agar tambah semangat Bikin tools baru ^_^"
+sleep 2
+echo $i"Tertarik dengan toolsnya? SILAHKAN SHARE"
+echo $i"Semoga Berkunjung kembali"
+sleep 2
+echo $i"Dan jangan lupa Add Facebook admin Oke 👌"
+echo $i"Bye ^_^"
+sleep 2
+echo
+echo $cy"Thanks FOR WATCHING....!!!"
+exit
+fi
